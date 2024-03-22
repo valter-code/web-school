dfd39759556453a7fddc536ee99b81e7f0ec8404
kevinekarevano <kev1n.ek4revano@gmail.com>
2024-03-22 11:55:53 +0700

    perubahan amin mantap

--- index.php
+++ index.php
@@ -303,103 +303,7 @@
     <!-- FOOTER -->
 
 
-    <!-- LOGIN -->
-    <div id=" login" class="fixed flex justify-center items-center bg-black bg-opacity-75 z-50 top-0 w-screen h-screen hidden">
-        <form action="" class="bg-purple-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-0 border border-gray-100  p-12 h-[400px] flex flex-col items-center justify-center">
-            <div class="mb-10">
-                <h1 class="text-white text-center font-bold">SMK TRIMULIA JAKARTA</h1>
-
-                <a href="" class="absolute right-1 top-1">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-                    </svg>
-                </a>
-
-                <h2 class="text-white text-center text-sm font-light">LOGIN SISWA</h2>
-            </div>
-
-            <div class="border-b-2 mb-10">
-                <label for="siswa" class="text-white">Nama Siswa</label>
-                <input type="text" id="siswa" class="w-full bg-transparent border-none focus:ring-0 text-white autofill:bg-black">
-            </div>
-
-            <div class="border-b-2 mb-10 w-full">
-                <label for="pass" class="text-white">Password </label>
-                <input type="password" id="pass" class="w-full bg-transparent border-none focus:ring-0 text-white autofill:bg-n">
-            </div>
-
-            <button class="w-full mb-5 bg-violet-800 py-2 rounded-full font-bold text-white shadow-sm hover:scale-95 transition duration-150 hover:bg-violet-900">
-                LOGIN
-            </button>
-
-            <h1 class="text-white">Belum Punya Akun? <a onclick="popDaftar()" href="#" class="underline font-semibold">Daftar</a></h1>
-
-            <div class="w-60 absolute -z-50 brightness-50 opacity-15 top-20">
-                <img src="./assets/logo.png" alt="">
-            </div>
-        </form>
-    </div>
-    <!-- LOGIN END -->
-
-
-    <!-- DAFTAR -->
-    <div id="daftar" class="fixed flex justify-center items-center bg-black bg-opacity-75 z-50 top-0 w-screen h-screen hidden">
-        <form action="" class="bg-purple-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-0 border border-gray-100  p-12 h-[510px] flex flex-col items-center justify-center">
-            <div class="mb-10">
-                <h1 class="text-white text-center font-bold">SMK TRIMULIA JAKARTA</h1>
-
-                <a href="" class="absolute right-1 top-1">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-                    </svg>
-                </a>
-
-                <h2 class="text-white text-center text-sm font-light">DAFTAR SISWA</h2>
-            </div>
-
-            <div class="border-b-2 mb-10">
-                <label for="siswa" class="text-white font-semibold  ">Nama Siswa</label>
-                <input type="text" id="siswa" class="w-full bg-transparent border-none focus:ring-0 text-white">
-            </div>
-
-            <div class="w-full mb-10 border-b-2">
-                <label for="jurusan" class="text-white font-semibold">Jurusan</label>
-                <select name="" id="jurusan" class="w-full bg-transparent border-none focus:ring-0 ">
-                    <option value="" disabled class="bg-neutral-800 text-white font-semibold">Daftar Jurusan</option>
-                    <option value="" class="bg-neutral-800 text-white font-semibold">TKJ</option>
-                    <option value="" class="bg-neutral-800 text-white font-semibold">MP</option>
-                    <option value="" class="bg-neutral-800 text-white font-semibold">BD</option>
-                </select>
-            </div>
-
-            <div class="border-b-2 mb-10 w-full">
-                <label for="pass" class="text-white font-semibold">Password </label>
-                <input type="password" id="pass" class="w-full bg-transparent border-none focus:ring-0 text-white">
-            </div>
-
-            <button class="w-full mb-5 bg-violet-800 py-2 rounded-full font-bold text-white shadow-sm hover:scale-95 transition duration-150 hover:bg-violet-900">
-                DAFTAR
-            </button>
-
-            <h1 class="text-white">Sudah Punya Akun? <a onclick="popLogin()" href="#" class="underline font-semibold">Login</a></h1>
-
-            <div class="w-64 absolute -z-50 brightness-50 opacity-15 top-50">
-                <img src="./assets/logo.png" alt="">
-            </div>
-        </form>
-    </div>
-    <!-- DAFTAR -->
-
-
-
-
-
-
-
-
-
     <!-- TO TOP -->
-
     <a id="toTop" href="#" class="hidden h-14 w-14 hover:animate-pulse bg-zinc-400  z-50 bottom-4 right-4 p-4 fixed rounded-full">
         <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m5 15 7-7 7 7" />
@@ -412,9 +316,6 @@
         </svg>
 
     </a>
-
-
-
 
     <!-- TO TOP END -->
 
